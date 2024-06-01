d582c775f2c67afa688b4de7180316976780da64
gusrama18 <gusrama18@gmail.com>
2024-06-01 13:52:54 +0000

    add discount and purchase price

--- lib/data/models/product.dart
+++ lib/data/models/product.dart
@@ -9,10 +9,13 @@ class ProductModel {
   String? skuNumber;
   String? description;
   String? photoUrl;
+  int? purchasePrice;
   int? price;
   String? unit;
   DateTime? createdAt;
   DateTime? updatedAt;
+  bool? isFavorite;
+  int? discount;
 
   ProductModel({
     required this.id,
@@ -20,11 +23,14 @@ class ProductModel {
     this.otherName,
     this.description,
     this.photoUrl,
+    this.purchasePrice,
     this.price,
     this.unit,
     this.skuNumber,
     this.createdAt,
     this.updatedAt,
+    this.isFavorite,
+    this.discount,
   }) {
     unit ??= "Rp.";
   }
@@ -39,11 +45,14 @@ class ProductModel {
     otherName: json["other_name"],
     description: json["description"],
     photoUrl: json["photoURL"],
+    purchasePrice: json["purchase_price"],
     price: json["price"],
     unit: json["unit"],
     skuNumber: json["sku_number"],
     createdAt: json["created_at"] != null ? DateTime.parse(json["created_at"]) : null,
     updatedAt: json["created_at"] != null ? DateTime.parse(json["updated_at"]) : null,
+    isFavorite: json["is_favorite"],
+    discount: json["discount"],
   );
 
   Map<String, dynamic> toJson() => {
@@ -52,11 +61,14 @@ class ProductModel {
     "other_name": otherName,
     "description": description,
     "photoURL": photoUrl,
+    "purchase_price": purchasePrice,
     "price": price,
     "unit": unit,
     "sku_number": skuNumber,
     "created_at": createdAt?.toIso8601String(),
     "updated_at": updatedAt?.toIso8601String(),
+    "is_favorite": isFavorite,
+    "discount": discount,
   };
 
   // Mapping method from ProductEntity
@@ -72,6 +84,7 @@ class ProductModel {
       skuNumber: entity.skuNumber,
       createdAt: entity.createdAt,
       updatedAt: entity.updatedAt,
+      discount: entity.discount,
     );
   }
 
@@ -83,11 +96,13 @@ class ProductModel {
       otherName: otherName ?? '',
       description: description ?? '',
       photoUrl: photoUrl ?? '',
+      purchasePrice: purchasePrice ?? 0,
       price: price ?? 0,
       unit: unit ?? 'RP',
       skuNumber: skuNumber,
       createdAt: createdAt,
       updatedAt: updatedAt,
+      discount: discount ?? 0,
     );
   }
 }
--- lib/domain/entities/product.dart
+++ lib/domain/entities/product.dart
@@ -5,10 +5,12 @@ class ProductEntity {
   String? skuNumber;
   String? description;
   String? photoUrl;
+  int? purchasePrice;
   int? price;
   String? unit;
   DateTime? createdAt;
   DateTime? updatedAt;
+  int? discount;
 
   ProductEntity({
     required this.id,
@@ -16,10 +18,27 @@ class ProductEntity {
     this.otherName,
     this.description,
     this.photoUrl,
+    this.purchasePrice,
     this.price,
     this.unit,
     this.skuNumber,
     this.createdAt,
     this.updatedAt,
+    this.discount,
   });
+
+  // calculate price with discount if discount is set
+  int getPrice() {
+    if(price == null) { return 0; }
+    if (discount != null) {
+      return (price! * (100 - discount!) / 100).toInt();
+    }
+    return price!;
+  }
+
+  // get the profit value based on the price (minus the discount if set) decrease by purchase price
+  int getProfit() {
+    if(purchasePrice == null) { return getPrice(); }
+    return getPrice() - purchasePrice!;
+  }
 }
